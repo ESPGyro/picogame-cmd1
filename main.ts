f52bbe1419d92abd1a3871c940bc37392480e0ff
ESP Gyro <99704174+ESPGyro@users.noreply.github.com>
2024-06-14 23:04:39 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ namespace BLECommandProcessor {
      * @param startChar Start character of the loop
      * @param endChar End character of the loop
      */
-    //% blockId="expandCommand" block="Convert BLE command %s"
+    //% blockId="expandCommand" block="Convert BLE command |input %s"
     export function expandCommand(s: string): string {
         return loopAdd(s, '5', '6');
     }
